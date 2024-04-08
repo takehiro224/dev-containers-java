fae9c725ed13ac26279d857a39cac4544b74bc05
takehiro.watanabe <mogu.224@gmail.com>
2024-04-08 16:28:02 +0900

    Initial commit.

---
+++ src/main/Hello.java
@@ -0,0 +1,12 @@
+package main;
+
+import java.time.LocalDateTime;
+
+public class Hello {
+    public static void main(String[] args) {
+        LocalDateTime time = LocalDateTime.now();
+        System.out.println(time);
+    }
+
+    int[] list = new int[]{1, 2};
+}
